f4969ac5f0bfce067a12c8f17fb77b911106d910
toth152 <55102105+toth152@users.noreply.github.com>
2021-09-16 16:14:48 +0200

    1.heti feladat (k02)

---
+++ WTX6LW_0908/feldolgozo.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML >
+<html lang = "hu">
+
+<head>
+	<title>Feldolgozo php</title>
+	<meta charset="UTF-8">
+</head>
+
+<body>
+
+<font style="font-size:16pt">
+<h2>HTML űrlap</h2>
+
+<?php
+
+echo ("<strong>Név:</strong> "  . $_POST['nev'] . "<br><br>");
+echo ("<strong>PIN kód:</strong> " . $_POST['kod'] . "<br><br>");
+echo ("<strong>Gyümölcs:</strong> " . $_POST['gyumolcs'] . "<br><br>");
+echo ("<strong>Életkor:</strong> " . $_POST['eletkor'] . "<br><br>");
+echo ("<strong>Lábméret:</strong> " . $_POST['labmeret'] . "<br><br>");
+echo ("<strong>Önbizalom:</strong> " . $_POST['onbizalom'] . "<br><br>");
+ 
+?>
+<a href="urlap.html">vissza az űrlapra...</a>
+
+</font>
+
+</body>
+</html>
